f06a8c303b1ebeea810cbf8886e5d145eed107b7
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-29 11:57:06 -0400

    test: add template HTML

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,11 +1,29 @@
-import express, { Router } from 'express';
+import fs from 'fs';
+import express from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
 
-const router = Router();
-router.get('/', (req, res) => res.send('Hello World!'));
+const server = async () => {
+  let template;
 
-app.use('*', router);
+  app.use('*', async (req, res) => {
+    const url = req.originalUrl;
+    const safeUrl = url === '/' ? '/index' : req.originalUrl;
+
+    try {
+      template = fs.readFileSync('./public/index.html', 'utf-8');
+
+      const html = template.replace(
+        '<!--ssr-outlet-->',
+        `<pre>${JSON.stringify({ url: url, safeUrl: safeUrl }, null, 2)}</pre>`
+      );
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    } catch (error) {}
+  });
+};
+
+server();
 
 export const handler = serverless(app);
