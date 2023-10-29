b28109ddcfb8bc9692cb003f9577f5545bf92ccc
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-29 12:59:29 -0400

    test: inline html template

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,22 +1,23 @@
-import fs from 'fs';
 import express from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
+const template =
+  '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Index</title></head><body><div id="app"><!--ssr-outlet--></div></body></html>';
 
 const server = async () => {
-  let template;
-
   app.use('*', async (req, res) => {
     const url = req.originalUrl;
     const safeUrl = url === '/' ? '/index' : req.originalUrl;
 
     try {
-      template = fs.readFileSync('./public/index.html', 'utf-8');
-
       const html = template.replace(
         '<!--ssr-outlet-->',
-        `<pre>${JSON.stringify({ url: url, safeUrl: safeUrl }, null, 2)}</pre>`
+        `<div>
+           <strong>url: ${url}</strong>
+           <strong>safeUrl: ${safeUrl}</strong>
+           <pre>${JSON.stringify({ url: url, safeUrl: safeUrl }, null, 2)}</pre>
+        </div>`
       );
 
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
