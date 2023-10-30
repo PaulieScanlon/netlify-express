537e717706f9084190cd853971def579daffffc2
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-29 20:20:53 -0400

    chore: delete build files from function

--- netlify/functions/functions/page-2/function.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const GET = async () => {
-  const request = await fetch("https://dummyjson.com/carts?limit=10");
-  const data = await request.json();
-  return data;
-};
-exports.GET = GET;
